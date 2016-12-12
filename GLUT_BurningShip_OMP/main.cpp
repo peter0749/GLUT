4f8b8f2d100815077124edc8b290ca710115f4d1
peter <jengku@gmail.com>
2016-12-12 09:24:40 +0800

    Fixed Colormap

--- GLUT_BurningShip_OMP/main.cpp
+++ GLUT_BurningShip_OMP/main.cpp
@@ -12,8 +12,7 @@
 #include <algorithm>
 #include <complex>
 #include <stack>
-#include <omp.h>
-#define PALETTE_SIZE 512
+#define PALETTE_SIZE 768
 #define EPS 0.99997f
 #define MP std::make_pair
 #define DEF_W 1280
@@ -29,6 +28,7 @@ size_t width=DEF_W, height=DEF_H;
 GLfloat minX = -4.0f, maxX = 4.0f, minY = -2.25f, maxY = 2.25f;
 GLfloat dX = ((maxX-minX)/(GLfloat)width)*EPS, dY = ((maxY-minY)/(GLfloat)height)*EPS;
 double oridX;
+unsigned int hist[PALETTE_SIZE+3];
 GLfloat palette[PALETTE_SIZE][3];//R,G,B;
 GLfloat black[3] = {0.0f,0.0f,0.0f};
 GLfloat white[3] = {1.0f,1.0f,1.0f};
@@ -37,6 +37,7 @@ bool FullScreen=false;
 char title[STRING_LEN],GMPbuffer[STRING_LEN];
 int consoleID;
 double old_dx=0, old_dy=0, new_dx=0, new_dy=0;
+double gam=5.0000f;
 int zoomstep=2;
 std::stack< PIV > HistoryWindow;
 int index_map[MAX_H][MAX_W];
@@ -51,15 +52,13 @@ const GLfloat rYIQ[3][3]= {
     {1.0f, -0.272, -0.647},
     {1.0f, -1.106, 1.703}
 };
-inline void slow_YIQ2RGB( GLfloat mtx[3] ){
-    GLfloat temp[3];
-    memset(temp,0x00,sizeof(temp));
+inline void slow_YIQ2RGB( GLfloat target[3], GLfloat mtx[3] ){
+    for(int i=0; i<3; ++i) target[i]=(GLfloat)0.0f;
     for(int k=0; k<3; ++k){
         for(int i=0; i<3; ++i){
-            temp[i] += rYIQ[i][k] *  mtx[k];
+            target[i] += rYIQ[i][k] *  mtx[k];
         }
     }
-    memcpy(mtx, temp, std::min(sizeof(mtx),sizeof(temp)));
 }
 
 inline void refresh_diff(void){
@@ -71,24 +70,16 @@ inline double mu_trans(double x, double mu){
 }
 
 void InitPalette(void){
-    GLfloat oriI=0.0f, oriQ=0.5226f, oriY=0.0f;
-    const double stepQ = 0.5226f*2.0f/(double)PALETTE_SIZE;
+    GLfloat oriI=-0.5957f, oriQ=0.5226f;
+    const double stepQ = 0.5226f*0.6f/(double)PALETTE_SIZE;
     const double stepI = 0.5957f*2.0f/(double)PALETTE_SIZE;
-    const double stepY = 1.0f/(double)PALETTE_SIZE;
 
     /*Use YIQ, Q Channel.*/
     for(int i=0; i<PALETTE_SIZE; ++i){
-        palette[i][0] = pow(i*stepY,0.3f);//Y
-        //palette[i][0] = (GLfloat)mu_trans((double)(i/PALETTE_SIZE), 255.0f);
-        palette[i][1] = (GLfloat)(stepI*(double)i);//I
+        //palette[i][0] = 1.0f;//Undetermined
+        palette[i][1] = oriI+(GLfloat)(stepI*(double)i);//I
         palette[i][2] = oriQ-(GLfloat)(stepQ*(double)i);//Q
     }
-    /*Convert YIQ to RGB*/
-    for(int i=0; i<PALETTE_SIZE; ++i){
-        slow_YIQ2RGB(palette[i]);
-    }
-    for(int i=0; i<3; ++i)
-        palette[0][i] = black[i];
 }
 
 void comput_ite(int h, int w, double pY, double pX, double minY, double minX){
@@ -108,12 +99,12 @@ void comput_ite(int h, int w, double pY, double pX, double minY, double minX){
                     break;
                 }
                 nr=nr-ni+x;
-                ni=std::fabs(R*I);
+                ni=fabs(R*I);
                 ni+=ni;
                 ni+=y;
                 if( (float)ni==(float)I && (float)nr==(float)R ){
 #ifdef VIS_REUSE
-                    index_map[i][j] = PALETTE_SIZE-1;
+                    index_map[i][j] = TH_HOLD;
 #endif
                     break;
                 };
@@ -134,22 +125,41 @@ inline void zoomInFunc(int key){//1 or 0//9
     maxY = maxY - dY*movey;
 }
 
+void det_luminance(void){
+#pragma omp parallel for
+    for(int i=1; i<PALETTE_SIZE; ++i) hist[i]+=hist[i-1];
+    for(int i=0; i<PALETTE_SIZE; ++i){
+        palette[i][0] = pow((double)hist[i] / (double)hist[PALETTE_SIZE-1], gam);//Determine Luminance
+    }
+}
+
 void Paint(void){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
     glBegin(GL_POINTS);
     double pY=(double)( (long double)(maxY-minY)/(long double)height );
     double pX=(double)( (long double)(maxX-minX)/(long double)width );
     comput_ite(height, width, pY, pX, minY, minX);
+    double Q = (double)TH_HOLD / (double)PALETTE_SIZE;
+    memset(hist,0x00,sizeof(hist));
+    for(int i=0; i<height; ++i)for(int j=0; j<width; ++j) ++hist[ (unsigned)((double)index_map[i][j] / Q) ];
+    det_luminance();
+    GLfloat setedCOLOR[3];
+    //for(int i=0; i<3; ++i) palette[0][i] = 0; //black
     for(int i=0; i<height; ++i){
         for(int j=0; j<width; ++j){
             GLfloat y = (GLfloat)((long double)pY*(long double)i+(long double)minY);
             GLfloat x = (GLfloat)((long double)pX*(long double)j+(long double)minX);
-            glColor3fv( palette[index_map[i][j]]);
+            /*Convert YIQ to RGB*/
+            if(!index_map[i][j]) glColor3fv(black);
+            else{
+                slow_YIQ2RGB(setedCOLOR, palette[ (unsigned)((double)index_map[i][j] / Q) ]);
+                glColor3fv( setedCOLOR );
+            }
             glVertex2f(x,y);
         }
     }
     glEnd();
-    sprintf(title,"X:%.2e Y:%.2e zoom(log2):%lf iterations:%d movestep:%d", (double)(minX+maxX)/2, (double)(minY+maxY)/2, log2(oridX/(double)dX), TH_HOLD, zoomstep);
+    sprintf(title,"X:%.2e Y:%.2e zoom(log2):%lf iterations:%d movestep:%d gamma:%.3lf", (double)(minX+maxX)/2, (double)(minY+maxY)/2, log2(oridX/(double)dX), TH_HOLD, zoomstep, gam);
     glDisable( GL_DEPTH_TEST);
 
     glColor3d(1.0,1.0,1.0);
@@ -294,6 +304,14 @@ void keyEvent(unsigned char key, int x, int y){ // function to handle key pressi
         case '7':
             if(zoomstep<1e9)
                 ++zoomstep;
+            break;
+        case '/':
+            gam+=0.01f;
+            break;
+        case '*':
+            if(gam>0.05f){
+                gam-=0.01f;
+            }
             break;
         case 27 : // escape key - close the program
             glutDestroyWindow(consoleID);
